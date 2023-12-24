76d293a3831db9465f129103b98b3627f7390b4a
skeana <skean2000@outlook.com>
2023-12-24 20:34:00 +0000

    #

--- scr/groups.ts
+++
@@ -1,7 +0,0 @@
-export interface Group {
-    id: string;
-    name: string;
-    members: string[];
-  }
-  
-  export const groups: Group[] = []; 
--- scr/romms.ts
+++
@@ -1,9 +0,0 @@
-export interface Room {
-    id: string;
-    name: string;
-    groupId: string;
-    chatHistory: string[];
-  }
-  
-  export const rooms: Room[] = []; 
-  
